8cf51887cae8c957132513125a68e4508c49b3d5
Tim Coulter <tim@Tims-MacBook-Pro.local>
2015-04-15 16:59:55 -0700

    Automatically compile assets; I tried to get this during `docker build`, but couldn't. Might have to come up with a different strategy to compile the assets, store the asset files, then inject them into redis on server start.

--- config/run.sh
+++ config/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 cd /src
+
+# Compile the assets if they're not yet compiled.
+# Do this in parallel with server start. 
+lines="`redis-cli keys \* | wc -l`"
+if [ $lines == "1" ]
+  then
+    ./compile_assets.coffee &
+fi
+
 ./server.coffee
